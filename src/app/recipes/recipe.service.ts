1f6f0f0b91b82bb98a6c44b654a9327488400c00
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-03 13:11:48 -0800

    adding ingdients to the recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,13 +10,21 @@ export class RecipeService {
       'Pizza',
       'Looks delicious!',
       // tslint:disable-next-line:max-line-length
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+      [
+        new Ingredient('Cheese', 1),
+        new Ingredient('Crust', 20)
+      ]
     ),
     new Recipe(
       'More Pizza!',
       'Looks delicious!',
       // tslint:disable-next-line:max-line-length
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+      [
+        new Ingredient('Cheese', 20),
+        new Ingredient('Crust', 23)
+      ]
     )
   ];
 
