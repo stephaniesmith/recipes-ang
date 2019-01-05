6a878bafda39dca59c833445acc30f823fd4a364
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-05 12:09:24 -0800

    edit mode

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+        this.id = parseInt(params['id'], 0);
+        this.editMode = params['id'] != null;
+      });
   }
 
 }
