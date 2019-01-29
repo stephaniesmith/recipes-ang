f4b88b69912eb72336f7106864813cc5fa810ed4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-29 08:08:39 -0800

    Deleting a  recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -55,4 +55,9 @@ export class RecipeService {
     this.recipeChanged.next(this.recipes.slice());
   }
 
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
 }
