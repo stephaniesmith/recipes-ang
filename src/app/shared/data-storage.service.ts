0d5ae5f074ccca96b66c1fd56715b73704c65ae4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-01 08:38:18 -0800

    fetching data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -8,5 +9,13 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://alchemy-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    return this.http.get('https://alchemy-recipe-book.firebaseio.com/recipes.json')
+    .subscribe((response: Response) => {
+      const recipes: Recipe[] = response.json();
+      this.recipeService.setRecipes(recipes);
+    });
   }
 }
