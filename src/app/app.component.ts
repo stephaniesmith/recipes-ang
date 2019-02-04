1e3f20ef595599a1003db8dcf6ee7291846c5953
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-04 07:56:03 -0800

    beginning to setup firebase auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipes-ang';
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAt85omcCXGgl2ioD3CdpE3Mg3bq5QnCMs',
+      authDomain: 'alchemy-recipe-book.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
