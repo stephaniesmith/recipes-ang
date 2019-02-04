2e5877ca4f292dcfe245543da4051ac27bbe3e64
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-04 08:44:50 -0800

    token stuff

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,17 +3,26 @@ import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(
+    private http: Http,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
-    return this.http.put('https://alchemy-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put(`https://alchemy-recipe-book.firebaseio.com/recipes.json?auth=${token}`, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://alchemy-recipe-book.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get(`https://alchemy-recipe-book.firebaseio.com/recipes.json?auth=${token}`)
       .pipe(map((response: Response) => {
         return response.json().map((recipe: Recipe) => {
           if (!recipe.ingredients) {
