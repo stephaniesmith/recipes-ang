8dd58b291bb164eac7e09a6dcf97ed06d315b594
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-06 08:18:14 -0800

    more module work

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
@@ -12,6 +15,10 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
     RecipeListComponent,
     RecipeEditComponent,
     RecipeDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class RecipesModule {}
