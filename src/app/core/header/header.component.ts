fbffa01562f07a1c813a00bb4fdc822693e3560f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-12 07:52:23 -0800

    removing the type

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
 import { Response } from '@angular/http';
 import { AuthService } from '../../auth/auth.service';
+import { HttpEvent } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -17,7 +18,7 @@ export class HeaderComponent {
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
-      .subscribe((response: Response) => {
+      .subscribe((response) => {
         console.log(response);
       });
   }
