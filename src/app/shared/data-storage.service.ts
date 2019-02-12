96082f31f8bc4f83e79475a9d26bdaf365c3ac49
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-12 07:59:36 -0800

    setting params

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -17,13 +17,21 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.httpClient.put(`https://alchemy-recipe-book.firebaseio.com/recipes.json?auth=${token}`, this.recipeService.getRecipes());
+    return this.httpClient.put('https://alchemy-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+      observe: 'body',
+      params: new HttpParams().set('auth', token)
+      // headers: new HttpHeaders().set('Authorization', 'Bearer askdfj')
+    });
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    return this.httpClient.get<Recipe[]>(`https://alchemy-recipe-book.firebaseio.com/recipes.json?auth=${token}`)
+    return this.httpClient.get<Recipe[]>('https://alchemy-recipe-book.firebaseio.com/recipes.json', {
+      observe: 'body',
+      responseType: 'json',
+      params: new HttpParams().set('auth', token)
+    })
       .pipe(map(recipes => {
         return recipes.map((recipe: Recipe) => {
           if (!recipe.ingredients) {
