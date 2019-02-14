139e250d03b6c826c0326d49acae00993095b8bc
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-14 08:25:36 -0800

    centralizing the state

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,7 +1,13 @@
-import { State as ShoppingListState } from '../shopping-list/store/shopping-list.reducers';
-import { State as AuthState } from '../auth/store/auth.reducer';
+import { State as ShoppingListState, shoppingListReducer } from '../shopping-list/store/shopping-list.reducers';
+import { State as AuthState, authReducer } from '../auth/store/auth.reducer';
+import { ActionReducerMap } from '@ngrx/store';
 
 export interface AppState {
   shoppingList: ShoppingListState;
   auth: AuthState;
 }
+
+export const reducers: ActionReducerMap<AppState> = {
+  shoppingList: shoppingListReducer,
+  auth: authReducer
+};
