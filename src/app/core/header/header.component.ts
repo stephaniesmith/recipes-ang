d5f2bbfef4c5f88a3eceea0f15a698245cd8e5d3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-14 08:36:35 -0800

    header update

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
-import { Response } from '@angular/http';
 import { AuthService } from '../../auth/auth.service';
-import { HttpEvent } from '@angular/common/http';
+import { Store, State } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { Observable } from 'rxjs';
+import { State as AuthState } from '../../auth/store/auth.reducer';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<AuthState>;
 
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<AppState>
   ) {}
+
+  ngOnInit() {
+    this.authState = this.store.select('auth');
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
