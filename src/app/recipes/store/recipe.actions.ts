5c08d66eaffa3363752271ec01201c8ba0012b6a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-19 08:47:45 -0800

    recipe reducers

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -2,9 +2,9 @@ import { Action } from '@ngrx/store';
 import { Recipe } from '../recipe.model';
 
 export const SET_RECIPES = 'SET_RECIPES';
-export const ADD_RECIPES = 'ADD_RECIPES';
-export const UPDATE_RECIPES = 'UPDATE_RECIPES';
-export const DELETE_RECIPES = 'DELETE_RECIPES';
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const UPDATE_RECIPE = 'UPDATE_RECIPE';
+export const DELETE_RECIPE = 'DELETE_RECIPE';
 
 export class SetRecipes implements Action {
   readonly type = SET_RECIPES;
@@ -12,22 +12,22 @@ export class SetRecipes implements Action {
   constructor(public payload: Recipe[]) {}
 }
 
-export class AddRecipes implements Action {
-  readonly type = ADD_RECIPES;
+export class AddRecipe implements Action {
+  readonly type = ADD_RECIPE;
 
   constructor(public payload: Recipe) {}
 }
 
-export class UpdateRecipes implements Action {
-  readonly type = UPDATE_RECIPES;
+export class UpdateRecipe implements Action {
+  readonly type = UPDATE_RECIPE;
 
   constructor(public payload: { index: number, updatedRecipe: Recipe}) {}
 }
 
-export class DeleteRecipes implements Action {
-  readonly type = DELETE_RECIPES;
+export class DeleteRecipe implements Action {
+  readonly type = DELETE_RECIPE;
 
   constructor(public payload: number) {}
 }
 
-export type RecipeActions = SetRecipes | AddRecipes | UpdateRecipes | DeleteRecipes;
+export type RecipeActions = SetRecipes | AddRecipe | UpdateRecipe | DeleteRecipe;
--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,5 +1,8 @@
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { RecipeActions, SET_RECIPES, ADD_RECIPES, ADD_RECIPE, UPDATE_RECIPE, DELETE_RECIPE } from './recipe.actions';
+import { S_IWOTH } from 'constants';
+import { Actions } from '@ngrx/effects';
 
 export interface FeatureState {
   recipes: State;
@@ -34,6 +37,26 @@ const initialState: State = {
   ]
 };
 
-export function recipeReducer(state = initialState, action) {
-
+export function recipeReducer(state = initialState, action: RecipeActions) {
+  switch (action.type) {
+    case SET_RECIPES:
+      return { ...state, recipes: [...action.payload] };
+    case ADD_RECIPE:
+      return { ...state, recipes: [...state.recipes, action.payload] };
+    case UPDATE_RECIPE:
+      const recipe = state.recipes[action.payload.index];
+      const updatedRecipe = {
+        ...recipe,
+        ...action.payload.updatedRecipe
+      };
+      const recipes = [...state.recipes];
+      recipes[action.payload.index] = updatedRecipe;
+      return { ...state, recipes };
+    case DELETE_RECIPE:
+      const oldRecipes = [...state.recipes];
+      oldRecipes.splice(action.payload, 1);
+      return { ...state, recipes: oldRecipes };
+    default:
+      return state;
+  }
 }
