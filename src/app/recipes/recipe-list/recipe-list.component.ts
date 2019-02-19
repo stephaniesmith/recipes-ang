2b5997a6f341d7df8c2612432fa4e437cd971e46
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-19 09:00:13 -0800

    cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 
